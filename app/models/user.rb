cfc130a4e1e9a0c94b1af114b1e29ab3e88db875
Naoko <naokodeveloper@gmail.com>
2021-01-18 10:43:18 -0600

    Basic user model with secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,13 @@
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
+    has_secure_password
     before_save {email.downcase!}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :name,   :email, presence: true
-    validates :name,   length: {maximum: 50}
-    validates :email,  length: {maximum: 255},
-                       format: {with: VALID_EMAIL_REGEX},
-                       uniqueness: true
+    validates :name, :password, :email, presence: true
+    validates :name,        length: {maximum: 50}
+    validates :password,    length: {minimum: 6}
+    validates :email,       length: {maximum: 255},
+                            format: {with: VALID_EMAIL_REGEX},
+                            uniqueness: true
 end
